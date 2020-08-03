47fc1df3ef4a89c5ceaff8bc6f00b9ea8a047b93
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-03 23:12:47 +0530

    start

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1> hello world</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
